09f81bfbf13ff0699431888b2f1b351ddcad53ad
Khushi Mehta <khushimehta82339@gmail.com>
2025-11-07 14:08:52 +0530

    Add node_modules and uploads folders

--- controllers/LabController.js
+++ controllers/LabController.js
@@ -36,18 +36,8 @@ exports.getLabTests = async (req, res) => {
 
 // Add test result
 // Add test result
-const LabTest = require('../models/LabTest');
-const Patient = require('../models/Patient');
-const LabPayment = require('../models/LabPayment');
-const Staff = require('../models/Staff');
 
-const fs = require('fs');
-const path = require('path');
 
-const uploadDir = path.join(__dirname, '../uploads/labReports');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 // Add test result
 exports.addTestResult = async (req, res) => {
