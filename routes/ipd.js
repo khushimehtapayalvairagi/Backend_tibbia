23a49cbe3b8b835aec37f07d519073cd9ad4bde8
Khushi Mehta <khushimehta82339@gmail.com>
2025-12-18 23:43:15 +0530

    all done

--- routes/ipd.js
+++ routes/ipd.js
@@ -7,6 +7,6 @@ router.get('/admissions/:patientId', ipdController.getIPDAdmissionsByPatient);
 router.put('/admissions/:id/discharge', ipdController.dischargeIPDAdmission);
 router.post('/reports', ipdController.createDailyProgressReport);
 router.get('/reports', ipdController.getDailyReportsByAdmission);
-router.get('/admissions/current-count', getAdmittedPatientsCount);
+router.get('/admissions/current-count', ipdController.getAdmittedPatientsCount);
 
 module.exports = router;
