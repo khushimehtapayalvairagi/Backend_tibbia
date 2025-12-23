4207d00b5c19c244efd57b48e9ab1a1f4402eb5c
Khushi Mehta <khushimehta82339@gmail.com>
2025-12-23 22:51:02 +0530

    all done

--- utils/sockets.js
+++ utils/sockets.js
@@ -1,40 +1,42 @@
-const { Server } = require('socket.io');
+const { Server } = require("socket.io");
 
 let io;
 
 const setupSocket = (server) => {
-    io = new Server(server, {
-        cors: {
-           origin: ["http://localhost:3000"],
-            // origin: ["http://localhost:3000", "https://uudra.in"],
-            credentials: true
-        }
+  io = new Server(server, {
+    cors: {
+      origin: [
+        "http://localhost:3000",
+        "https://tibbia.kloudcrm.site",
+        //  "https://kloudcrm.site",
+      ],
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+  });
+
+  io.on("connection", (socket) => {
+    console.log("🟢 Socket connected:", socket.id);
+
+    socket.on("joinDoctorRoom", (doctorId) => {
+      socket.join(`doctor_${doctorId}`);
+      console.log(`Doctor joined: doctor_${doctorId}`);
     });
 
-    io.on('connection', (socket) => {
-        console.log('User connected:', socket.id);
-
-        socket.on('joinDoctorRoom', (doctorId) => {
-            socket.join(`doctor_${doctorId}`);
-            console.log(`Doctor ${doctorId} joined room doctor_${doctorId}`);
-        });
-
-        socket.on('joinReceptionistRoom', () => {
-            socket.join('receptionist_room');
-            console.log('A receptionist joined the receptionist_room');
-        });
-
-        socket.on('disconnect', () => {
-            console.log('User disconnected:', socket.id);
-        });
+    socket.on("joinReceptionistRoom", () => {
+      socket.join("receptionist_room");
+      console.log("Receptionist joined room");
     });
+
+    socket.on("disconnect", () => {
+      console.log("🔴 Socket disconnected:", socket.id);
+    });
+  });
 };
 
 const getIO = () => {
-    if (!io) {
-        throw new Error('Socket.io not initialized!');
-    }
-    return io;
+  if (!io) throw new Error("Socket.io not initialized!");
+  return io;
 };
 
 module.exports = { setupSocket, getIO };
