12a5f6e4d5f72183daf8100af673039133e85b1f
Khushi Mehta <khushimehta82339@gmail.com>
2025-12-31 16:12:04 +0530

    all done

--- models/Staff.js
+++ models/Staff.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const StaffSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    contactNumber: { type: String, required: true, unique:true },
+    contactNumber: { type: String},
     designation: { type: String, enum: ['Head Nurse', 'Lab Technician', 'Receptionist','Inventory Manager', 'Other'], required: true },
     department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department' },
     isActive: { type: Boolean, default: true }   
